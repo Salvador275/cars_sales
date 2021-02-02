2f90ef182d027deed730d473a466362485106c98
sardor <sardor@sunweb.pro>
2021-02-03 00:19:02 +0500

    init

---
+++ src/main/java/com/carbiz/ahan/AhanApplication.java
@@ -0,0 +1,13 @@
+package com.carbiz.ahan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carbiz/ahan/AhanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carbiz.ahan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
